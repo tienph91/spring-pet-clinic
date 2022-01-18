503f6ecad56d6d74400ae077dd75ddf1ab88fb80
tienph <tienph@fabbi.io>
2022-01-18 21:26:57 +0700

    Get bean by SpringContext

--- src/main/java/com/example/petclinic/PetClinicApplication.java
+++ src/main/java/com/example/petclinic/PetClinicApplication.java
@@ -1,13 +1,24 @@
 package com.example.petclinic;
 
+import com.example.petclinic.controller.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class PetClinicApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PetClinicApplication.class, args);
+        ApplicationContext applicationContext = SpringApplication.run(PetClinicApplication.class, args);
+
+        MyController myController = applicationContext.getBean(MyController.class);
+        MyController myController1 = (MyController) applicationContext.getBean("myController");
+
+        myController.sayHello();
+        myController1.sayHello();
+
+        System.out.println(myController);
+        System.out.println(myController1);
     }
 
 }
