0d88b26d06551dc333a98bc354860e108ebe87f8
tienph91 <hoangtien.cp@gmail.com>
2022-01-20 21:42:49 +0700

    default profile

--- src/main/java/com/example/petclinic/configuration/MailConfiguration.java
+++ src/main/java/com/example/petclinic/configuration/MailConfiguration.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("EN")
+@Profile({"EN", "default"})
 public class MailConfiguration {
 
     @Bean("mail")
