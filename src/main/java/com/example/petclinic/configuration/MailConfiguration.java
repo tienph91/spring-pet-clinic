e88334b01421bfb1b523f128fa4452dfe7976d70
tienph91 <hoangtien.cp@gmail.com>
2022-01-21 09:50:26 +0700

    assignment 4

--- src/main/java/com/example/petclinic/configuration/MailConfiguration.java
+++ src/main/java/com/example/petclinic/configuration/MailConfiguration.java
@@ -3,13 +3,19 @@ package com.example.petclinic.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
 
-@Configuration
+@Component
 @Profile({"ES", "default"})
 public class MailConfiguration {
 
     @Bean("mail")
     public Mail getMail() {
+        return new Mail();
+    }
+
+    @Bean("mail1")
+    public Mail getMail1() {
         return new Mail();
     }
 }
