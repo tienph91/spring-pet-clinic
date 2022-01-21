5318d84d4dbe8bfd21b36f1319f1388dea8250ee
tienph91 <hoangtien.cp@gmail.com>
2022-01-21 16:28:20 +0700

    Flashcard

--- src/main/java/com/example/petclinic/controller/MyController.java
+++ src/main/java/com/example/petclinic/controller/MyController.java
@@ -1,10 +1,10 @@
 package com.example.petclinic.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
-
     public String sayHello() {
         System.out.println("Hello the world!!!");
 
