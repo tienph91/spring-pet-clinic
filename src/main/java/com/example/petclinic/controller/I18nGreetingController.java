9560f7a3f7ac8a171c3035647d0fc813d8a29acb
tienph91 <hoangtien.cp@gmail.com>
2022-01-21 11:36:06 +0700

    spring bean life cycle

--- src/main/java/com/example/petclinic/controller/I18nGreetingController.java
+++ src/main/java/com/example/petclinic/controller/I18nGreetingController.java
@@ -1,11 +1,15 @@
 package com.example.petclinic.controller;
 
 import com.example.petclinic.service.GreetingService;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Controller;
 
 @Controller
-public class I18nGreetingController {
+public class I18nGreetingController implements InitializingBean, DisposableBean, BeanNameAware, BeanFactoryAware, ApplicationContextAware {
 
     private final GreetingService greetingService;
 
@@ -15,5 +19,30 @@ public class I18nGreetingController {
 
     public String sayHello() {
         return greetingService.sayGreeting();
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println(this);
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        this.setBeanName("abc");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println(this);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println(this);
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println(this);
     }
 }
